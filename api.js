c942e10603afb39b6202d70f57b69d0560fdfab5
Yoonieharrington <126143682+Yoonieharrington@users.noreply.github.com>
2025-11-26 23:21:20 +0000

    Fix formatting in API fetch response handling

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 fetch('https://express-backend-9cfm.onrender.com/lessons')
   .then((res) => res.json())
   .then((data) => {
-    this.lessons = data // or however you store lessons
+    this.lessons = data 
   })
